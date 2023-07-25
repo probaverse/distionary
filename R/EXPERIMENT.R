270759d01867f1d1e702bc341a38143713716894
Vincenzo Coia <vincenzo.coia@gmail.com>
2023-07-24 22:50:02 -0700

    comment out experiment

--- R/EXPERIMENT.R
+++ R/EXPERIMENT.R
@@ -1,10 +1,10 @@
-density <- new_representation(connects_to = "cdf")
-
-eval_density <- new_eval_fun(representation = density)
-eval_bi_density <- new_eval_bi_fun(representation = density)
-
-enframe_density <- new_enframe_uni(representation)
-
-# Fall back
-eval_representation.default
+# density <- new_representation(connects_to = "cdf")
+#
+# eval_density <- new_eval_fun(representation = density)
+# eval_bi_density <- new_eval_bi_fun(representation = density)
+#
+# enframe_density <- new_enframe_uni(representation)
+#
+# # Fall back
+# eval_representation.default
 
