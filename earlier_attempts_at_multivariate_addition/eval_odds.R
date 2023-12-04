d60eecc11a8f47571d2bafb2461d1c5c230ebdbd
Vincenzo Coia <vincenzo.coia@gmail.com>
2023-12-04 09:29:38 -0800

    add in earlier attempts at adding multivariate distributions from March 2023

---
+++ earlier_attempts_at_multivariate_addition/eval_odds.R
@@ -0,0 +1,30 @@
+#' Odds Function
+#'
+#' Access a distribution's odds function. The odds of an event having
+#' probability `p` is `p / (1 - p)`.
+#'
+#' @inheritParams eval_cdf
+#' @return The evaluated odds in vector form (for `eval_`) and data frame
+#' or tibble form (for `enframe_`).
+#' @examples
+#' d <- dst_pois(1)
+#' eval_odds(d, at = c(1, 2, 2.5))
+#' enframe_odds(d, at = 0:4)
+#' @family distributional representations
+#' @rdname odds
+#' @export
+eval_odds <- function(distribution, ...) {
+	p <- eval_pmf(distribution, ...)
+	p / (1 - p)
+}
+
+#' @rdname odds
+#' @export
+enframe_odds <- function(..., at, arg_name = ".arg", fn_prefix = "odds",
+						 sep = "_") {
+	enframe_general(..., at = at, arg_name = arg_name, fn_prefix = fn_prefix,
+					sep = sep, eval_fn = eval_odds)
+}
+
+
+enframe_odds <- function(...) enframe_general(..., eval_fn = eval_odds)
