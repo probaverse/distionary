3a64e4f07761d972b6bfedd2d9b7a196d9d8ee93
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-02-23 23:44:04 -0800

    Vectorize p/d/q functions for GEV and GPD; stricter control on inputs

--- R/pdqr_gev.R
+++ R/pdqr_gev.R
@@ -2,83 +2,136 @@
 #'
 #' @param x,q Vector of quantiles.
 #' @param p Vector of probabilities.
-#' @param location Location parameter; single numeric.
-#' @param scale Scale parameter; single positive numeric.
-#' @param shape Shape parameter; single numeric.
+#' @param location Location parameter; numeric vector.
+#' @param scale Scale parameter; positive numeric vector.
+#' @param shape Shape parameter; numeric vector.
 #' This is also the extreme value index,
 #' so that `shape > 0` is heavy tailed, and `shape < 0` is short-tailed.
-#' @returns Vector of evaluated GEV distribution.
+#' @returns Vector of evaluated GEV distribution, with length
+#' equal to the recycled lengths of `q`/`x`/`p`, `location`, `scale`, and
+#' `shape`.
 #' @examples
 #' pgev(1:10, 0, 1, 1)
-#' dgev(1:10, 0, 2, 0)
+#' dgev(1:10, 1:10, 2, 0)
 #' qgev(1:9 / 10, 2, 10, -2)
 #' @rdname gev_raw
 #' @export
 pgev <- function(q, location, scale, shape) {
-  r <- gev_range(location, scale, shape)
+  checkmate::assert_numeric(q)
+  checkmate::assert_numeric(location)
+  checkmate::assert_numeric(scale)
+  checkmate::assert_numeric(shape)
+  l <- vctrs::vec_recycle_common(q, location, scale, shape)
+  q <- l[[1]]
+  location <- l[[2]]
+  scale <- l[[3]]
+  shape <- l[[4]]
+  lo <- gev_lower(location, scale, shape)
+  hi <- gev_upper(location, scale, shape)
   t <- gev_t_function(q, location = location, scale = scale, shape = shape)
   res <- exp(-t)
-  res[q <= r[1L]] <- 0
-  res[q >= r[2L]] <- 1
+  res[q <= lo] <- 0
+  res[q >= hi] <- 1
   res
 }
 
 #' @rdname gev_raw
-#' @inheritParams pgev
 #' @export
 qgev <- function(p, location, scale, shape) {
-  invalid_at <- p < 0 | p > 1
-  if (shape == 0) {
-    res <- location - scale * log(-log(p))
-  } else {
-    res <- location + scale * ((-log(p))^(-shape) - 1) / shape
-  }
-  res[invalid_at] <- NaN
-  res
+  checkmate::assert_numeric(p)
+  checkmate::assert_numeric(location)
+  checkmate::assert_numeric(scale)
+  checkmate::assert_numeric(shape)
+  l <- vctrs::vec_recycle_common(p, location, scale, shape)
+  p <- l[[1]]
+  location <- l[[2]]
+  scale <- l[[3]]
+  shape <- l[[4]]
+  i_zero <- shape == 0
+  i_non0 <- shape != 0
+  res <- numeric(length(p))
+  neglogp <- -log(p)
+  res[i_zero] <- -log(neglogp[i_zero])
+  res[i_non0] <- (neglogp[i_non0]^(-shape[i_non0]) - 1) / shape[i_non0]
+  res[p < 0 | p > 1] <- NaN
+  location + scale * res
 }
 
 #' @rdname gev_raw
-#' @inheritParams pgev
 #' @export
 dgev <- function(x, location, scale, shape) {
-  r <- gev_range(location, scale, shape)
+  checkmate::assert_numeric(x)
+  checkmate::assert_numeric(location)
+  checkmate::assert_numeric(scale)
+  checkmate::assert_numeric(shape)
+  l <- vctrs::vec_recycle_common(x, location, scale, shape)
+  x <- l[[1]]
+  location <- l[[2]]
+  scale <- l[[3]]
+  shape <- l[[4]]
+  lo <- gev_lower(location, scale, shape)
+  hi <- gev_upper(location, scale, shape)
   t <- gev_t_function(x, location = location, scale = scale, shape = shape)
   res <- t^(shape + 1) / scale * exp(-t)
-  res[x <= r[1L]] <- 0
-  res[x > r[2L]] <- 0
+  res[x <= lo] <- 0
+  res[x > hi] <- 0
   res
 }
 
 #' 't()' function for calculating GEV quantities
 #'
+#' Decides whether to use `exp()` when the shape parameter is 0,
+#' or the non-limiting form otherwise. Also useful for the GPD.
+#'
 #' @param x Argument of the function; vectorized.
 #' @returns A vector of the t function evaluated.
-#' @inheritParams dst_gev
+#' @inheritParams pgev
 #' @seealso See the Wikipedia entry for the GEV distribution,
 #' https://en.wikipedia.org/wiki/Generalized_extreme_value_distribution
 #' @note The shape parameter is not vectorized. This function is only
 #' intended to be used when location, scale, and shape are scalars.
 #' @noRd
 gev_t_function <- function(x, location, scale, shape) {
+  l <- vctrs::vec_recycle_common(x, location, scale, shape)
+  x <- l[[1]]
+  location <- l[[2]]
+  scale <- l[[3]]
+  shape <- l[[4]]
   z <- (x - location) / scale
-  if (shape == 0) {
-    exp(-z)
-  } else {
-    (1 + shape * z)^(-1 / shape)
-  }
+  res <- numeric(length(x))
+  i_zero <- shape == 0
+  i_non0 <- shape != 0
+  res[i_zero] <- exp(-z[i_zero])
+  res[i_non0] <- (1 + shape[i_non0] * z[i_non0])^(-1 / shape[i_non0])
+  res
 }
 
 #' Range of a GEV distribution
 #'
-#' @inheritParams dst_gev
-#' @returns A vector of length 2 indicating the support of the distribution.
+#' @inheritParams pgev
+#' @returns A vector of mimima for `gev_lower()`, or maxima
+#' for `gev_upper()`, corresponding to the input parameters.
 #' @noRd
-gev_range <- function(location, scale, shape) {
-  if (shape > 0) {
-    c(location - scale / shape, Inf)
-  } else if (shape == 0) {
-    c(-Inf, Inf)
-  } else if (shape < 0) {
-    c(-Inf, location - scale / shape)
-  }
+gev_lower <- function(location, scale, shape) {
+  l <- vctrs::vec_recycle_common(location, scale, shape)
+  location <- l[[1]]
+  scale <- l[[2]]
+  shape <- l[[3]]
+  res <- rep(-Inf, length(location))
+  i_calc <- shape > 0
+  res[i_calc] <- location[i_calc] - scale[i_calc] / shape[i_calc]
+  res
+}
+
+#' @inheritParams gev_lower
+#' @noRd
+gev_upper <- function(location, scale, shape) {
+  l <- vctrs::vec_recycle_common(location, scale, shape)
+  location <- l[[1]]
+  scale <- l[[2]]
+  shape <- l[[3]]
+  res <- rep(Inf, length(location))
+  i_calc <- shape < 0
+  res[i_calc] <- location[i_calc] - scale[i_calc] / shape[i_calc]
+  res
 }
--- R/pdqr_gpd.R
+++ R/pdqr_gpd.R
@@ -2,94 +2,100 @@
 #'
 #' @param x,q Vector of quantiles.
 #' @param p Vector of probabilities.
-#' @param location Location parameter to shift the GPD by.
-#' @param scale Scale parameter; single positive numeric.
-#' @param shape Shape parameter; single positive numeric.
-#' @param lower.tail Logical. If `TRUE`, cdf (default);
+#' @param scale Vector of scale parameters; positive numeric.
+#' @param shape Vector of shape parameters; positive numeric.
+#' @param lower.tail Single logical. If `TRUE`, cdf (default);
 #' if `FALSE`, survival function.
-#' @returns Vector of evaluated GPD distribution.
+#' @returns Vector of evaluated GPD distribution, with length
+#' equal to the recycled lengths of `q`/`x`/`p`, `scale`, and `shape`.
 #' @examples
-#' pgpd(1:10, 0, 1, 1)
-#' dgpd(1:10, 0, 2, 0)
-#' qgpd(1:9 / 10, 2, 10, -2)
+#' pgpd(1:10, 1, 1)
+#' dgpd(1:10, 2, 0)
+#' qgpd(1:9 / 10, 10, -2)
 #' @rdname gpd_raw
 #' @export
-pgpd <- function(q, location, scale, shape, lower.tail = TRUE) {
-  if (shape == 0) {
-    left <- q < location
-    z <- (q - location) / scale
-    if (lower.tail) {
-      res <- 1 - exp(-z)
-      res[left] <- 0
-    } else {
-      res <- exp(-z)
-      res[left] <- 1
-    }
-    res
+pgpd <- function(q, scale, shape, lower.tail = TRUE) {
+  checkmate::assert_numeric(q)
+  checkmate::assert_numeric(scale)
+  checkmate::assert_numeric(shape)
+  checkmate::assert_logical(lower.tail, len = 1, any.missing = FALSE)
+  l <- vctrs::vec_recycle_common(q, scale, shape)
+  q <- l[[1]]
+  scale <- l[[2]]
+  shape <- l[[3]]
+  hi <- gpd_upper(scale = scale, shape = shape)
+  left <- q < 0
+  right <- q > hi
+  t <- gev_t_function(q, location = 0, scale = scale, shape = shape)
+  if (lower.tail) {
+    res <- 1 - t
+    res[left] <- 0
+    res[right] <- 1
   } else {
-    if (shape > 0) {
-      rightend <- Inf
-    } else {
-      rightend <- location - scale / shape
-    }
-    left <- q < location
-    right <- q > rightend
-    z <- (q - location) / scale
-    if (lower.tail) {
-      res <- 1 - (1 + shape * z)^(-1 / shape)
-      res[left] <- 0
-      res[right] <- 1
-    } else {
-      res <- (1 + shape * z)^(-1 / shape)
-      res[left] <- 1
-      res[right] <- 0
-    }
-    res
+    res <- t
+    res[left] <- 1
+    res[right] <- 0
   }
+  res
 }
 
 
 #' @rdname gpd_raw
-#' @inheritParams pgpd
 #' @export
-qgpd <- function(p, location, scale, shape) {
-  invalid <- p < 0 | p > 1
-  if (shape == 0) {
-    res <- location - scale * log(1 - p)
-    res[invalid] <- NaN
-    res
-  } else {
-    if (shape > 0) {
-      rightend <- Inf
-    } else {
-      rightend <- location - scale / shape
-    }
-    t <- 1 / (1 - p)
-    res <- location + scale * (t^shape - 1) / shape
-    res[invalid] <- NaN
-    res
-  }
+qgpd <- function(p, scale, shape) {
+  # z = (RP^xi - 1) / xi -> log(RP)
+  checkmate::assert_numeric(p)
+  checkmate::assert_numeric(scale)
+  checkmate::assert_numeric(shape)
+  l <- vctrs::vec_recycle_common(p, scale, shape)
+  p <- l[[1]]
+  scale <- l[[2]]
+  shape <- l[[3]]
+  rp <- 1 / (1 - p)
+  res <- numeric(length(p))
+  i_zero <- shape == 0
+  i_non0 <- shape != 0
+  res[i_zero] <- log(rp[i_zero])
+  res[i_non0] <- (rp[i_non0]^shape[i_non0] - 1) / shape[i_non0]
+  res[p < 0 | p > 1] <- NaN
+  scale * res
 }
 
 #' @rdname gpd_raw
-#' @inheritParams pgpd
 #' @export
-dgpd <- function(x, location, scale, shape) {
-  z <- (x - location) / scale
-  if (shape == 0) {
-    outside <- x < location
-    res <- exp(-z) / scale
-    res[outside] <- 0
-    res
-  } else {
-    if (shape > 0) {
-      rightend <- Inf
-    } else {
-      rightend <- location - scale / shape
-    }
-    outside <- x < location | x > rightend
-    res <- (1 + shape * z)^(-1 / shape - 1) / scale
-    res[outside] <- 0
-    res
-  }
+dgpd <- function(x, scale, shape) {
+  checkmate::assert_numeric(x)
+  checkmate::assert_numeric(scale)
+  checkmate::assert_numeric(shape)
+  l <- vctrs::vec_recycle_common(x, scale, shape)
+  x <- l[[1]]
+  scale <- l[[2]]
+  shape <- l[[3]]
+  z <- x / scale
+  hi <- gpd_upper(scale = scale, shape = shape)
+  outside <- x < 0 | x > hi
+  t <- gev_t_function(x, location = 0, scale = scale, shape = shape)
+  i_zero <- shape == 0
+  i_non0 <- shape != 0
+  res <- numeric(length(x))
+  res[i_zero] <- t[i_zero]
+  res[i_non0] <- (1 + shape[i_non0] * z[i_non0])^(-1 / shape[i_non0] - 1)
+  res[outside] <- 0
+  res / scale
+}
+
+
+#' Upper endpoint of GPD
+#'
+#' @inheritParams dst_gpd
+#' @returns A vector of maxima corresponding to the input parameters.
+#' @noRd
+gpd_upper <- function(scale, shape) {
+  l <- vctrs::vec_recycle_common(scale, shape)
+  scale <- l[[1]]
+  shape <- l[[2]]
+  res <- rep(Inf, length(scale))
+  i_calc <- shape < 0
+  res[i_calc] <- -scale[i_calc] / shape[i_calc]
+  res
 }
