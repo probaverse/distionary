0d6341c4b4463e3fe33cbec3805a2946e4f5dd23
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-03-25 09:42:26 -0700

    test parameter setting

--- tests/testthat/test-parameters.R
+++ tests/testthat/test-parameters.R
@@ -6,5 +6,11 @@ test_that("Parameters are specified and retrieved correctly w parameters()", {
     d <- rlang::exec(item$distribution, !!!paramset)
     param_retrieved[[i]] <- parameters(d)
     expect_identical(param_retrieved[[!!i]], !!paramset)
+    parameters(d)[[1]] <- "hello"
+    expect_identical(parameters(d)[[1]], "hello")
+    parameters(d)[[1]] <- NaN
+    expect_identical(parameters(d)[[1]], NaN)
+    parameters(d)[[1]] <- data.frame(x = 1)
+    expect_identical(parameters(d)[[1]], data.frame(x = 1))
   }
 })
